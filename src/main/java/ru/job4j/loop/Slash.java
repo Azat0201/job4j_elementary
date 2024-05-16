a878f11739f0887952b04b2c677053a3f8d0320f
snake <azatismanskij@gmail.com>
2024-05-16 21:23:54 +0300

    5.9. Крест в псевдографике.  [#177182 #422361].

--- src/main/java/ru/job4j/loop/Slash.java
+++ src/main/java/ru/job4j/loop/Slash.java
@@ -2,26 +2,21 @@ package ru.job4j.loop;
 
 public class Slash {
     public static void draw(int size) {
-        for (int row = 0; row < size; row++) {
-            for (int cell = 0; cell < size; cell++) {
-                boolean left = row == cell;
-                boolean right = row == size - cell - 1;
-                if (left) {
-                    System.out.print("0");
-                } else if (right) {
-                    System.out.print("0");
-                } else {
-                    System.out.print(" ");
-                }
-            }
-            System.out.println();
+        int abs_i;
+        int half_size = size / 2;
+        for (int i = -half_size; i <= half_size; i++) {
+            abs_i = Math.abs(i);
+            System.out.print(" ".repeat(half_size - abs_i) + "0");
+            if (i != 0)
+                System.out.print(" ".repeat(abs_i * 2 - 1) + "0");
+            System.out.println(" ".repeat(half_size - abs_i));
         }
         System.out.println();
     }
 
     public static void main(String[] args) {
         System.out.println("Draw by 3");
-        draw(3);
+        draw(4);
         System.out.println("Draw by 5");
         draw(5);
     }
