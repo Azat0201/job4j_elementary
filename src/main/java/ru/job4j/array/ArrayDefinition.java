e4d7e5921010961a13ce42d81c60f430c750ba21
snake <azatismanskij@gmail.com>
2024-05-17 23:56:28 +0300

    6.0.3. Заполнение массива. [#226300 #422370]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,14 @@ public class ArrayDefinition {
         System.out.println(ages.length);
         System.out.println(surnames.length);
         System.out.println(prices.length);
+
+        String[] names = new String[4];
+        names[0] = "Ivan";
+        names[1] = "Petr";
+        names[2] = "Nikita";
+        names[3] = "Sergey";
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
     }
 }
